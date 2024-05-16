47af11c923548b2d26bf811ca18e001b79593354
Jose Bogarin <josejbogarin13@gmail.com>
2024-05-16 10:32:49 -0300

    primer commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+
+export const images = {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'aceternity.com',
+      // You can add these as well
+      // port: '',
+      // pathname: 'arifscloud/image/upload/**',
+    },
+  ],
+};
